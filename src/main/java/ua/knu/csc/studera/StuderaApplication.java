3c67938c270dfef4892520b1b6836398f1df4ac6
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-05-17 17:33:00 +0300

    initial commit

---
+++ src/main/java/ua/knu/csc/studera/StuderaApplication.java
@@ -0,0 +1,13 @@
+package ua.knu.csc.studera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuderaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuderaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/knu/csc/studera/StuderaApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.knu.csc.studera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuderaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
